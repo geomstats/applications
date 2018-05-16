1d5decf1d14cbcacc98a67d1fd37eaf159d8a7d9
donnate <cdonnat@stanford.edu>
2018-05-16 10:46:22 -0700

    Updates

--- MRI_geom/SPD_fMRI.py
+++ MRI_geom/SPD_fMRI.py
@@ -1,13 +1,12 @@
-import numpy as np
-import np.linalg as lin
+import geomstats.spd_matrices_space as spd_space
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import time
 from sklearn.svm import SVC
 from sklearn.model_selection import KFold
-from sklearn.metrics import precision_score, accuracy_score
-from sklearn.metrics import f1_score, recall_score, confusion_matrix
-import geomstats.spd_matrices_space as spd_space
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import f1_score, precision_score, recall_score 
+import time
 
 DIM_SPACE = 28
 RAND_SEED = 2018
@@ -19,19 +18,18 @@ SIGMAS = list(np.arange(0.5, 20, 0.5))
 
 
 def import_data():
-    graphs = pd.DataFrame.from_csv("data/train_FNC.csv")
-    n_graphs, _ = graphs.shape
-    map_FCN = pd.DataFrame.from_csv("AdditionalInformation/comp_ind_fMRI.csv",
+    graphs = pd.DataFrame.from_csv("data/train_fnc.csv")
+    map_FCN = pd.DataFrame.from_csv("additional_info/comp_ind_fmri.csv",
                                     index_col=None)
     map_FCN = map_FCN['fMRI_comp_ind'].to_dict()
     map_FCN_r = {v: k for k, v
                  in map_FCN.iteritems()}
-    mapping = pd.DataFrame.from_csv("AdditionalInformation/" +
-                                    "rs_fMRI_FNC_mapping.csv")
+    mapping = pd.DataFrame.from_csv("additional_info/" +
+                                    "rs_fmri_fnc_mapping.csv")
     graph_labels = pd.DataFrame.from_csv("data/train_labels.csv")
-    all_graphs = [None]*n_graphs
-    all_targets = [None]*n_graphs
-    for i in range(n_graphs):
+    all_graphs = [None] * n_graphs
+    all_targets = [None] * n_graphs
+    for i in range(NB_GRAPHS):
         u = np.zeros((DIM_SPACE, DIM_SPACE))
         for ii in range(mapping.shape[0]):
             a, b = (mapping.iloc[ii]["mapA"], mapping.iloc[ii]["mapB"])
@@ -41,17 +39,17 @@ def import_data():
         all_targets[i] = graph_labels.loc[graphs.index[i], "Class"]
 
     np.random.seed(RAND_SEED)
-    index_train = range(len(all_graphs))
+    index_train = range(NB_GRAPHS)
     np.random.shuffle(index_train)
-    stop = int(0.85 * len(all_graphs))
+    stop = int(0.85 * NB_GRAPHS)
     labels = np.array(all_targets[index_train])
     return graphs.iloc[:, index_train], [all_graphs[t] for t in index_train],\
         labels, stop, index_train
 
 
-def laplacian(A):
-    D = np.diag(np.array(A.sum(1)).flatten())
-    return D-A
+def laplacian(a):
+    d = np.diag(np.array(a.sum(1)).flatten())
+    return d-a
 
 
 def fit_kernel_cv(log_euclidean_distance, labels,
@@ -64,7 +62,7 @@ def fit_kernel_cv(log_euclidean_distance, labels,
         distance = np.exp(- np.square(log_euclidean_distance)/(sigma**2))
     else:
         distance = log_euclidean_distance
-    for train_index, test_index in kf.split(range(len(labels))):
+    for train_index, test_index in kf.split(range(NB_GRAPHS)):
         train_index = np.array(train_index)
         test_index = np.array(test_index)
         x = np.array(distance[train_index, :])[:, train_index]
@@ -94,23 +92,23 @@ def main():
     tic = time.time()
     for option in ["LED", "ED", "RD"]:
         tic = time.time()
-        for i in range(1, len(all_graphs)):
+        for i in range(1, NB_GRAPHS):
             hat_l1 = laplacian(all_graphs[i]) + gamma*np.eye(DIM_SPACE)
-            Lambd, U = lin.eigh(hat_l1)
+            Lambd, U = np.linalg.eigh(hat_l1)
             invL1 = U.dot(np.diag(1.0/np.sqrt(Lambd)).dot(U.T))
             for j in range(i):
                 hat_l2 = laplacian(all_graphs[j]) + gamma*np.eye(DIM_SPACE)
                 if option == "LED":
-                    distance[option][i, j] = lin.norm(spd_space.group_log(hat_l1).squeeze(0)
-                                                      - spd_space.group_log(hat_l2).squeeze(0),
-                                                      'fro')
+                    distance[option][i, j] = np.linalg.norm(spd_space.group_log(hat_l1).squeeze(0)
+                                                            - spd_space.group_log(hat_l2).squeeze(0),
+                                                            'fro')
                 elif option == "ED":
-                    distance[option][i, j] = lin.norm(hat_l1
-                                                      - hat_l2, 'fro')
+                    distance[option][i, j] = np.linalg.norm(hat_l1
+                                                            - hat_l2, 'fro')
                 else:
-                    distance[option][i, j] = lin.norm(
-                                             invL1.dot(spd_space.group_log(hat_l2).squeeze(0).dot(invL1)),
-                                             'fro')
+                    distance[option][i, j] = np.linalg.norm(
+                                                            invL1.dot(spd_space.group_log(hat_l2).squeeze(0).dot(invL1)),
+                                                            'fro')
         distance[option] = distance[option] + distance[option].T
         toc = time.time()
         time_alg[option] = toc-tic
@@ -157,17 +155,16 @@ def main():
 if __name__ == "__main__":
 
     distance, _, perf_final, sigma_chosen, mean_acc, labels = main()
-    sigmas = SIGMAS
     print("Final performance of the model on OOS test data:")
     print(pd.DataFrame.from_dict(perf_final))
 
     # Plot CV accuracy
     fig, ax = plt.subplots(figsize=(6, 4))
-    plt.plot(sigmas, mean_acc['LED'][1:],
+    plt.plot(SIGMAS, mean_acc['LED'][1:],
              label="Log-Euclidean \nDistance", c="blue")
-    plt.plot(sigmas, mean_acc['ED'][1:],
+    plt.plot(SIGMAS, mean_acc['ED'][1:],
              label="Euclidean \nDistance", c="black")
-    plt.plot(sigmas, mean_acc['RD'][1:],
+    plt.plot(SIGMAS, mean_acc['RD'][1:],
              label="Riemannian \nDistance", c="red")
     plt.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), fontsize=18)
     plt.xlabel(r'$\sigma$', fontsize=24)
