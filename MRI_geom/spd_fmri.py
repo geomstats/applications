e5982605fc4bad2706cfdf05f775426947e87eb4
donnate <cdonnat@stanford.edu>
2018-05-16 14:10:15 -0700

    Update spd_fmri.py

--- MRI_geom/spd_fmri.py
+++ MRI_geom/spd_fmri.py
@@ -15,18 +15,18 @@ CORR_THRESH = 0.1
 GAMMA = 1.0
 NB_GRAPHS = 86
 SIGMAS = list(np.arange(0.5, 20, 0.5))
-DISTANCE = ["log_euclidean", "frobenius", "riemannian"]
+DISTANCES = ['log_euclidean', 'frobenius', 'riemannian']
 
 def import_data():
-    graphs = pd.DataFrame.from_csv("data/train_fnc.csv")
-    map_FCN = pd.DataFrame.from_csv("add_info/comp_ind_fmri.csv",
-                                    index_col=None)
-    map_FCN = map_FCN['fMRI_comp_ind'].to_dict()
-    map_FCN_r = {v: k for k, v
-                 in map_FCN.iteritems()}
-    mapping = pd.DataFrame.from_csv("add_info/" +
-                                    "rs_fmri_fnc_mapping.csv")
-    graph_labels = pd.DataFrame.from_csv("data/train_labels.csv")
+    graphs = pd.DataFrame.from_csv('data/train_fnc.csv')
+    map_functional = pd.DataFrame.from_csv('add_info/comp_ind_fmri.csv',
+                                           index_col=None)
+    map_functional = map_functional['fMRI_comp_ind'].to_dict()
+    map_functional_r = {v: k for k, v
+                        in map_functional.iteritems()}
+    mapping = pd.DataFrame.from_csv('add_info/' +
+                                    'rs_fmri_fnc_mapping.csv')
+    graph_labels = pd.DataFrame.from_csv('data/train_labels.csv')
     all_graphs = [None] * NB_GRAPHS
     all_targets = [None] * NB_GRAPHS
     
@@ -34,15 +34,16 @@ def import_data():
         u = np.zeros((DIM_SPACE, DIM_SPACE))
         nb_edges = mapping.shape[0]
         for edge in range(nb_edges):
-            e0, e1 = (mapping.iloc[edge]["mapA"], mapping.iloc[edge]["mapB"])
-            u[map_FCN_r[e0], map_FCN_r[e1]] = graphs.iloc[graph_id][edge]
+            e0, e1 = (mapping.iloc[edge]['mapA'], mapping.iloc[edge]['mapB'])
+            region0, region1 = map_functional_r[e0], map_functional_r[e1]
+            u[region0, region1] = graphs.iloc[graph_id][edge]
             u = np.multiply(u, (u > CORR_THRESH))
         return np.abs(u + u.T)
                        
         
     for graph_id in range(NB_GRAPHS):
         all_graphs[graph_id] = create_connectome(graph_id, mapping)
-        all_targets[graph_id] = graph_labels.loc[graphs.index[graph_id], "Class"]
+        all_targets[graph_id] = graph_labels.loc[graphs.index[graph_id], 'Class']
 
     np.random.seed(RAND_SEED)
     index_train = range(NB_GRAPHS)
@@ -94,9 +95,9 @@ def main():
     time_alg = {}
     graphs, all_graphs, labels, stop, index_train = import_data()
     distance = {option: np.zeros((NB_GRAPHS, NB_GRAPHS))
-                for option in DISTANCE}
+                for option in DISTANCES}
     tic = time.time()
-    for option in DISTANCE:
+    for option in DISTANCES:
         tic = time.time()
         for c, g in enumerate(all_graphs):
             hat_l1 = laplacian(g) + gamma*np.eye(DIM_SPACE)
@@ -123,7 +124,7 @@ def main():
     print("Starting SVM fitting with Cross Validation")
     sigmas = SIGMAS
     mean_acc = {}
-    for option in DISTANCE:
+    for option in DISTANCES:
         print("Option: ", option)
         mean_acc[option] = []
         perf, conf = fit_kernel_cv(distance[option][:stop, :stop],
@@ -139,7 +140,7 @@ def main():
     model = {}
     perf_final = {}
     sigma_chosen = {}
-    for option in DISTANCE:
+    for option in DISTANCES:
         sigma_chosen[option] = sigmas[np.argmax(mean_acc[option])] - 1
         distance2 = np.exp(- np.square(distance[option])
                            / (sigma_chosen[option]**2))
@@ -166,7 +167,7 @@ if __name__ == "__main__":
 
     # Plot CV accuracy
     fig, ax = plt.subplots(figsize=(6, 4))
-    for option in DISTANCE:
+    for option in DISTANCES:
         plt.plot(SIGMAS, mean_acc['log_euclidean'][1:],
                  label=option + " \nDistance")
     plt.legend(loc="center left", bbox_to_anchor=(1.0, 0.5), fontsize=18)
@@ -176,7 +177,7 @@ if __name__ == "__main__":
     # Plot Clustermap representation
     import seaborn as sb
     import scipy.cluster.hierarchy as hc
-    for option in DISTANCE:
+    for option in DISTANCES:
         plt.figure()
         distance2 = np.exp(-np.square(distance[option])
                            / (sigma_chosen[option]**2))
