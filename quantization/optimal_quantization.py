6fc7401034ff8106aff9de05f5218a1b30df99a2
AliceLeBrigant <42436240+AliceLeBrigant@users.noreply.github.com>
2018-08-17 17:29:02 +0200

    Update of optimal_quantization.py
    Corrections of the script including new descriptions, implementation of the circle (will be useful in unit test) and addition of new outputs.

--- quantization/optimal_quantization.py
+++ quantization/optimal_quantization.py
@@ -1,56 +1,76 @@
 """
-Optimal quantization
+Optimal quantization of the empirical distribution of a dataset -
+
+The algorithm finds an optimal aproximation of the dataset by a
+smaller number n of points, and yields a K-means type clustering.
+The n points of the approximation, weighted by the masses of their
+Voronoi cells, define a discrete distribution that best approximates,
+in the sense of the Wasserstein distance and at fixed support size n,
+the empirical distribution of the dataset. The Voronoi diagram associated
+to these points yields a K-means type clustering of the dataset.
 """
 
 import numpy as np
 from geomstats.hypersphere import HypersphereMetric
 
 
-S2metric = HypersphereMetric(dimension=2)
-
 TOLERANCE = 1e-5
-IMPLEMENTED = ['S2']
+IMPLEMENTED = ('S1', 'S2')
 
 
 def sample_from(points, size=1):
     """
-    Sample from the empirical distribution associated to points
+    Sample from the empirical distribution associated to points.
     """
     n_points = points.shape[0]
     dimension = points.shape[-1]
 
-    ind = np.random.randint(low=0, high=n_points, size=size)
-    sample = points[np.ix_(ind, np.arange(dimension))]
+    index = np.random.randint(low=0, high=n_points, size=size)
+    sample = points[np.ix_(index, np.arange(dimension))]
 
     return sample
 
 
 def closest_neighbor(point, neighbors, space=None):
     """
-    Find closest neighbor of point among neighbors
+    Find closest neighbor of point among neighbors.
     """
-    if space == 'S2':
-        dist = S2metric.dist(point, neighbors)
-        ind_closest_neighbor = dist.argmin()
+    if space == 'S1':
+        metric = HypersphereMetric(dimension=1)
+    else:
+        metric = HypersphereMetric(dimension=2)
 
-    return ind_closest_neighbor
+    dist = metric.dist(point, neighbors)
+    index_closest_neighbor = dist.argmin()
+
+    return index_closest_neighbor
 
 
 def optimal_quantization(points, n_centers=10, space=None, n_repetition=20,
                          tolerance=TOLERANCE):
     """
     Compute the optimal approximation of points by a smaller number
-    n of weighted centers.
+    of weighted centers using the Competitive Learning Riemannian
+    Quantization algorithm. Returns :
+        - n_centers centers
+        - n_centers weights between 0 and 1
+        - an array containing the labels of a clustering
+        - the number of steps needed to converge.
     """
     if space not in IMPLEMENTED:
         raise NotImplementedError(
                 'The optimal quantization function is not implemented'
                 ' for space {}. The spaces available for quantization'
                 ' are: {}.'.format(space, IMPLEMENTED))
+    elif space == 'S1':
+        metric = HypersphereMetric(dimension=1)
+    else:
+        metric = HypersphereMetric(dimension=2)
+
     # random initialization of the centers
     centers = sample_from(points, n_centers)
 
-    gap = 1
+    gap = 1.0
     step = 0
 
     while gap > tolerance:
@@ -59,14 +79,25 @@ def optimal_quantization(points, n_centers=10, space=None, n_repetition=20,
 
         sample = sample_from(points)
 
-        ind = closest_neighbor(sample, centers, space)
-        center_to_update = centers[ind, :]
+        index = closest_neighbor(sample, centers, space)
+        center_to_update = centers[index, :]
 
-        if space == 'S2':
-            tan_vec_update = S2metric.log(sample, center_to_update) / (k+1)
-            new_center = S2metric.exp(tan_vec_update, center_to_update)
-            gap = S2metric.dist(center_to_update, new_center)
+        tan_vec_update = metric.log(sample, center_to_update) / (k+1)
+        new_center = metric.exp(tan_vec_update, center_to_update)
+        gap = metric.dist(center_to_update, new_center)
+        gap = (gap != 0) * gap + (gap == 0)
 
-        centers[ind, :] = new_center
+        centers[index, :] = new_center
 
-    return centers, step
+    n_points = points.shape[0]
+    clustering = np.zeros((n_points,))
+    weights = np.zeros((n_centers,))
+
+    for i, point in enumerate(points):
+        index = closest_neighbor(point, centers, space)
+        clustering[i] = index
+        weights[index] += 1
+
+    weights = weights / n_points
+
+    return centers, weights, clustering, step
