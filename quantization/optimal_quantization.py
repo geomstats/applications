54f7cb66a2eec8e8f0ae1b84d0379bc28330d300
AliceLeBrigant <42436240+AliceLeBrigant@users.noreply.github.com>
2018-08-29 17:48:09 +0200

    Update optimal_quantization.py
    Remove closest_neighbor, diameter and karcher_flow

--- quantization/optimal_quantization.py
+++ quantization/optimal_quantization.py
@@ -1,5 +1,5 @@
 """
-Optimal quantization of the empirical distribution of a dataset -
+Optimal quantization of the empirical distribution of a dataset.
 
 The algorithm finds an optimal aproximation of the dataset by a
 smaller number n of points, and yields a K-means type clustering.
@@ -10,7 +10,7 @@ the empirical distribution of the dataset. The Voronoi diagram associated
 to these points yields a K-means type clustering of the dataset.
 """
 
-import numpy as np
+import geomstats.backend as gs
 from geomstats.hypersphere import HypersphereMetric
 
 
@@ -25,104 +25,22 @@ def sample_from(points, size=1):
     n_points = points.shape[0]
     dimension = points.shape[-1]
 
-    index = np.random.randint(low=0, high=n_points, size=size)
-    sample = points[np.ix_(index, np.arange(dimension))]
+    index = gs.random.randint(low=0, high=n_points, size=size)
+    sample = points[gs.ix_(index, gs.arange(dimension))]
 
     return sample
 
 
-def closest_neighbor(point, neighbors, space=None):
-    """
-    Find closest neighbor of point among neighbors.
-    """
-    if space not in IMPLEMENTED:
-        raise NotImplementedError(
-                'The closest neighbor function is not implemented'
-                ' for space {}. The spaces available'
-                ' are: {}.'.format(space, IMPLEMENTED))
-    elif space == 'S1':
-        metric = HypersphereMetric(dimension=1)
-    else:
-        metric = HypersphereMetric(dimension=2)
-
-    dist = metric.dist(point, neighbors)
-    index_closest_neighbor = dist.argmin()
-
-    return index_closest_neighbor
-
-
-def diameter_of_data(points, space=None):
-    """
-    Compute the two points that are farthest away from each other in points.
-    """
-    if space not in IMPLEMENTED:
-        raise NotImplementedError(
-                'The diameter function is not implemented'
-                ' for space {}. The spaces available'
-                ' are: {}.'.format(space, IMPLEMENTED))
-    elif space == 'S1':
-        metric = HypersphereMetric(dimension=1)
-    else:
-        metric = HypersphereMetric(dimension=2)
-
-    diameter = 0.0
-    n_points = points.shape[0]
-
-    for i in range(n_points-1):
-        dist_to_neighbors = metric.dist(points[i, :], points[i+1:, :])
-        dist_to_farthest_neighbor = dist_to_neighbors.max()
-        diameter = np.maximum(diameter, dist_to_farthest_neighbor * 2)
-
-    return diameter
-
-
-def karcher_flow(points, space=None, tolerance=TOLERANCE):
-    """
-    Compute the Karcher mean of points using a Karcher flow algorithm.
-    Return :
-        - the karcher mean
-        - the number of steps needed to converge.
-    """
-    if space not in IMPLEMENTED:
-        raise NotImplementedError(
-                'The Karcher mean function is not implemented'
-                ' for space {}. The spaces available for Karcher flow'
-                ' are: {}.'.format(space, IMPLEMENTED))
-    elif space == 'S1':
-        metric = HypersphereMetric(dimension=1)
-    else:
-        metric = HypersphereMetric(dimension=2)
-
-    karcher_mean = sample_from(points)
-
-    n_points = points.shape[0]
-    gap = 1.0
-    step = 0
-    while gap > tolerance:
-        tangent_vectors = np.zeros(points.shape)
-
-        for i in range(n_points):
-            tangent_vectors[i, :] = metric.log(points[i, :], karcher_mean)
-
-        tan_vec_update = np.sum(tangent_vectors, axis=0) / n_points
-        karcher_mean_updated = metric.exp(tan_vec_update, karcher_mean)
-
-        gap = metric.dist(karcher_mean, karcher_mean_updated)
-        karcher_mean = karcher_mean_updated
-        step += 1
-
-    return karcher_mean, step
-
-
-def optimal_quantization(points, n_centers=10, space=None, n_repetition=20,
-                         tolerance=TOLERANCE):
+def optimal_quantization(points, n_centers=10, space=None, n_repetitions=20,
+                         tolerance=TOLERANCE, n_max_iterations=50000):
     """
     Compute the optimal approximation of points by a smaller number
     of weighted centers using the Competitive Learning Riemannian
-    Quantization algorithm. Returns :
+    Quantization algorithm. See https://arxiv.org/abs/1806.07605.
+    Return :
         - n_centers centers
         - n_centers weights between 0 and 1
-        - an array containing the labels of a clustering
+        - an array of labels defining a clustering
         - the number of steps needed to converge.
     """
     if space not in IMPLEMENTED:
@@ -139,33 +57,45 @@ def optimal_quantization(points, n_centers=10, space=None, n_repetition=20,
     centers = sample_from(points, n_centers)
 
     gap = 1.0
-    step = 0
+    iteration = 0
 
-    while gap > tolerance:
-        step += 1
-        k = np.floor(step / n_repetition) + 1
+    while iteration < n_max_iterations:
+        iteration += 1
+        step_size = gs.floor(iteration / n_repetitions) + 1
 
         sample = sample_from(points)
 
-        index = closest_neighbor(sample, centers, space)
+        index = metric.closest_neighbor(sample, centers)
         center_to_update = centers[index, :]
 
-        tan_vec_update = metric.log(sample, center_to_update) / (k+1)
-        new_center = metric.exp(tan_vec_update, center_to_update)
+        tangent_vec_update = metric.log(sample, center_to_update) \
+            / (step_size+1)
+        new_center = metric.exp(tangent_vec_update, center_to_update)
         gap = metric.dist(center_to_update, new_center)
         gap = (gap != 0) * gap + (gap == 0)
 
         centers[index, :] = new_center
 
+        if gs.isclose(gap, 0, atol=tolerance):
+                break
+
+    if iteration is n_max_iterations:
+        print('Maximum number of iterations {} reached. The'
+              'quantization may be inaccurate'.format(n_max_iterations))
+
     n_points = points.shape[0]
-    clustering = np.zeros((n_points,))
-    weights = np.zeros((n_centers,))
+    clusters = dict()
+    weights = gs.zeros((n_centers,))
+    index_list = list()
 
     for i, point in enumerate(points):
-        index = closest_neighbor(point, centers, space)
-        clustering[i] = index
+        index = metric.closest_neighbor(point, centers)
+        if index not in index_list:
+            clusters[index] = list()
+            index_list.append(index)
+        clusters[index].append(i)
         weights[index] += 1
 
     weights = weights / n_points
 
-    return centers, weights, clustering, step
+    return centers, weights, clusters, iteration
