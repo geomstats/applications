74933869de1019f21e65747be23e95345556ac9b
Alice Le Brigant <alice.le-brigant@enac.fr>
2018-09-18 19:48:43 +0200

    changes to plot_quantization_s2.py

--- quantization/plot_quantization_s2.py
+++ quantization/plot_quantization_s2.py
@@ -12,42 +12,35 @@ import quantization.optimal_quantization as oq
 from geomstats.hypersphere import Hypersphere
 
 SPHERE2 = Hypersphere(dimension=2)
+METRIC = SPHERE2.metric
+N_POINTS = 1000
+N_CENTERS = 5
+N_REPETITIONS = 20
+KAPPA = 10
 
 
 def main():
-    # generate points from the von Mises Fisher distribution
-    n_points = 1000
-    points = SPHERE2.random_von_mises_fisher(kappa=10, n_samples=n_points)
 
-    # find the 5 centers that best approximate the points
-    n_centers = 5
-    centers, weights, clustering, n_steps = oq.optimal_quantization(
-                points, n_centers, space='S2'
+    points = SPHERE2.random_von_mises_fisher(kappa=KAPPA, n_samples=N_POINTS)
+
+    centers, weights, clusters, n_steps = oq.optimal_quantization(
+                points=points, metric=METRIC, n_centers=N_CENTERS,
+                n_repetitions=N_REPETITIONS
                 )
+
     plt.figure(0)
     ax = plt.subplot(111, projection="3d", aspect="equal")
     visualization.plot(centers, ax, space='S2', c='r')
     plt.show()
 
-    # the Voronoi diagram associated to the centers yields a clustering
-    clusters = {}
-    for i in range(n_centers):
-        clusters[i] = points[clustering == i]
-
-    sphere = visualization.Sphere()
-
     plt.figure(1)
     ax = plt.subplot(111, projection="3d", aspect="equal")
-    color = np.random.rand(n_centers, 3)
-    ax.plot_wireframe(sphere.sphere_x,
-                      sphere.sphere_y,
-                      sphere.sphere_z,
-                      color="black", alpha=0.2)
-    for i in range(n_centers):
-        points_x = np.vstack([point[0] for point in clusters[i]])
-        points_y = np.vstack([point[1] for point in clusters[i]])
-        points_z = np.vstack([point[2] for point in clusters[i]])
-        ax.scatter(points_x, points_y, points_z,  c=color[i, :])
+    color = np.random.rand(N_CENTERS, 3)
+    for i in range(N_CENTERS):
+        sphere = visualization.Sphere()
+        cluster_i = np.vstack([point for point in clusters[i]])
+        sphere.add_points(cluster_i)
+        sphere.draw(ax=ax, c=color[i, :])
 
 
 if __name__ == "__main__":
