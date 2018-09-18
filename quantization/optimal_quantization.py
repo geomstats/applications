aeb232531bf87fbd6270b978819f16d420b9d4e3
Alice Le Brigant <alice.le-brigant@enac.fr>
2018-09-18 18:52:59 +0200

    changes to optimal_quantization.py

--- quantization/optimal_quantization.py
+++ quantization/optimal_quantization.py
@@ -11,49 +11,43 @@ to these points yields a K-means type clustering of the dataset.
 """
 
 import geomstats.backend as gs
-from geomstats.hypersphere import HypersphereMetric
 
 
 TOLERANCE = 1e-5
 IMPLEMENTED = ('S1', 'S2')
 
+# TODO(alice): Consider adding optimal_quantization
+# as a method of the class RiemannianMetric.
 
-def sample_from(points, size=1):
+
+def sample_from(points, n_samples=1):
     """
     Sample from the empirical distribution associated to points.
     """
     n_points = points.shape[0]
     dimension = points.shape[-1]
 
-    index = gs.random.randint(low=0, high=n_points, size=size)
+    index = gs.random.randint(low=0, high=n_points, size=(n_samples,))
     sample = points[gs.ix_(index, gs.arange(dimension))]
 
     return sample
 
 
-def optimal_quantization(points, n_centers=10, space=None, n_repetitions=20,
+def optimal_quantization(points, metric, n_centers=10, n_repetitions=20,
                          tolerance=TOLERANCE, n_max_iterations=50000):
     """
     Compute the optimal approximation of points by a smaller number
     of weighted centers using the Competitive Learning Riemannian
-    Quantization algorithm. See https://arxiv.org/abs/1806.07605.
+    Quantization algorithm. The centers are updated using decreasing
+    step sizes, each of which stays constant for n_repetitions iterations
+    to allow a better exploration of the data points.
+    See https://arxiv.org/abs/1806.07605.
     Return :
         - n_centers centers
         - n_centers weights between 0 and 1
         - a dictionary containing the clusters
         - the number of steps needed to converge.
     """
-    if space not in IMPLEMENTED:
-        raise NotImplementedError(
-                'The optimal quantization function is not implemented'
-                ' for space {}. The spaces available for quantization'
-                ' are: {}.'.format(space, IMPLEMENTED))
-    elif space == 'S1':
-        metric = HypersphereMetric(dimension=1)
-    else:
-        metric = HypersphereMetric(dimension=2)
-
-    # random initialization of the centers
     centers = sample_from(points, n_centers)
 
     gap = 1.0
@@ -63,14 +57,17 @@ def optimal_quantization(points, n_centers=10, space=None, n_repetitions=20,
         iteration += 1
         step_size = gs.floor(iteration / n_repetitions) + 1
 
-        sample = sample_from(points)
+        point = sample_from(points)
 
-        index = metric.closest_neighbor(sample, centers)
+        index = metric.closest_neighbor_index(point, centers)
         center_to_update = centers[index, :]
 
-        tangent_vec_update = metric.log(sample, center_to_update) \
-            / (step_size+1)
-        new_center = metric.exp(tangent_vec_update, center_to_update)
+        tangent_vec_update = metric.log(
+                point=point, base_point=center_to_update
+                ) / (step_size+1)
+        new_center = metric.exp(
+                tangent_vec=tangent_vec_update, base_point=center_to_update
+                )
         gap = metric.dist(center_to_update, new_center)
         gap = (gap != 0) * gap + (gap == 0)
 
@@ -79,7 +76,7 @@ def optimal_quantization(points, n_centers=10, space=None, n_repetitions=20,
         if gs.isclose(gap, 0, atol=tolerance):
                 break
 
-    if iteration is n_max_iterations:
+    if iteration == n_max_iterations-1:
         print('Maximum number of iterations {} reached. The'
               'quantization may be inaccurate'.format(n_max_iterations))
 
@@ -88,12 +85,12 @@ def optimal_quantization(points, n_centers=10, space=None, n_repetitions=20,
     weights = gs.zeros((n_centers,))
     index_list = list()
 
-    for i, point in enumerate(points):
-        index = metric.closest_neighbor(point, centers)
+    for point in points:
+        index = metric.closest_neighbor_index(point, centers)
         if index not in index_list:
             clusters[index] = list()
             index_list.append(index)
-        clusters[index].append(i)
+        clusters[index].append(point)
         weights[index] += 1
 
     weights = weights / n_points
